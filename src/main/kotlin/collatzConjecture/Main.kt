a8a59a235590f0abac4b33b094b58f1006c39c8e
Leandro <leo250703@gmail.com>
2022-09-30 10:59:55 -0400

    Commit 30/09/2022 10:59(Leandro Morales) -- conjetura de collazt

--- src/main/kotlin/collatzConjecture/Main.kt
+++ src/main/kotlin/collatzConjecture/Main.kt
@@ -1,13 +1,30 @@
 package collatzConjecture
 
 fun main() {
-    val i: Int
-    val j: Int
-
-    i = verifyValue()
-    j = verifyValue()
-
+    val i = verifyValue()
+    var j = verifyValue()
     
+    val collaztL = collatzLenght(i,j)
+
+    println(collaztL)
+}
+
+fun collatzLenght(i: Int, j: Int): Int {
+    val lenghts: ArrayList<Int> = ArrayList()
+    for (cont in i..j) {
+        lenghts.add(collatz(cont))
+    }
+    return lenghts.max()
+}
+
+fun collatz(cont: Int): Int {
+    var num = cont
+    var lenght = 1
+    while (num > 1){
+        if (num % 2 == 0) num /= 2 else num = (3 * num) + 1
+        lenght++
+    }
+    return lenght
 }
 
 fun verifyValue(): Int{
